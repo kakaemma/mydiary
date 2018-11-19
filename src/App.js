c0010357c264c77a28ab0d2fb892538194a195e7
kakaemma <kakaemma1@gmail.com>
2018-11-19 11:37:10 +0300

    chore(react app): set up react application - this setsups a react application [Deivers #162044714]

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './assets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Welcome <code>to</code> my Diary.
+          </p>
+            My diary application
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
